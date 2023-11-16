3457392203584e1f54e0cc35b8d1d59249b693ea
yoyodev577 <135641206+yoyow123@users.noreply.github.com>
2023-11-16 00:23:17 -0800

    fix the tree layering issues

---
+++ Unity/Assets/Scripts/SpriteRenderOrderSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteRenderOrderSystem : MonoBehaviour
+{
+    SpriteRenderer[] renderers;
+    // Start is called before the first frame update
+    void Start()
+    {
+       renderers = FindObjectsOfType<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (SpriteRenderer renderer in renderers) {
+            renderer.sortingOrder = (int)renderer.transform.position.y * -100;
+        }
+    }
+}
